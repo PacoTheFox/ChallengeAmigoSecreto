0260f11691bcb135c50ba34632a0b01d95124363
PacoTheFox <franaguisan011@gmail.com>
2025-01-28 19:26:24 -0600

    Challenge Completado

--- app.js
+++ app.js
@@ -1,1 +1,60 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let listaAmigos = [];
+
+function limpiarCaja() {
+    let valorCaja = document.querySelector('#amigo').value = '';
+}
+
+
+
+function agregarAmigo() {
+
+
+    let friendToAdd = document.getElementById("amigo").value;
+
+    if (friendToAdd == "") {
+        alert("Por favor ingrese un nombre válido");
+        return;
+    }
+
+    listaAmigos.push(friendToAdd);
+
+    actualizarListaAmigos();
+    limpiarCaja()
+
+
+}
+
+function actualizarListaAmigos() {
+    let ul = document.getElementById("listaAmigos");
+    ul.innerHTML = "";
+
+    listaAmigos.forEach(amigo => {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        ul.appendChild(li);
+    })
+}
+
+function selectAmigo() {
+
+
+    if (listaAmigos.length < 2) {
+        alert("La lista de amígos es muy corta, ingrese más nombres");
+        return;
+    }
+    
+    const randomNumber = Math.floor(Math.random() * listaAmigos.length);
+
+    let selectedAmigo = listaAmigos[randomNumber];
+
+    asignarTextoElemento("listaAmigos", "");
+    asignarTextoElemento("resultado", `El amigo sorteado es: ${selectedAmigo}`);
+}
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.getElementById(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
